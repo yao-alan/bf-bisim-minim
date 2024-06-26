bec2c654a2d7e1da54ab9f3bac61a13cd53db35d
Alan <alan.yao@ucla.edu>
2024-06-25 22:49:49 -0700

    Fetching NLTK subset of Penn Treebank corpus.

---
+++ treebank.py
@@ -0,0 +1,28 @@
+from nltk import Tree
+from nltk.corpus import treebank
+from os import mkdir
+from os.path import exists
+from collections import deque
+
+if not exists("./penn_treebank_subset"):
+    mkdir("./penn_treebank_subset")
+
+for fid in treebank.fileids():
+
+    parsed_sents = treebank.parsed_sents(fid)
+    for i, tree in enumerate(parsed_sents):
+
+        f = open(f"./penn_treebank_subset/{fid.split('.')[0]}-sentence-{i}", 'w')
+
+        n_subtrees = tree.subtrees(lambda t: t.height() == 5)
+        for st in n_subtrees:
+            stk = deque()
+            stk.append((st, 0))
+            while len(stk) > 0:
+                t, depth = stk.pop()
+                f.write("\t"*depth + f"{str(t.label())}\n")
+                for st in reversed(t):
+                    if type(st) == Tree:
+                        stk.append((st, depth+1))
+        
+        f.close()
